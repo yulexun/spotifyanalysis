b37fb4fa15b462b25e4c9aca317f26001e5abe40
Lexun Yu <lx.yu@mail.utoronto.ca>
2024-10-10 17:39:13 -0400

    update intro

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -11,34 +11,12 @@
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+PTP <- readRDS("data/01-raw_data/PTP.rds")
+radiohead <- readRDS("data/01-raw_data/radiohead.rds")
+boogie <- readRDS("data/01-raw_data/boogie.rds")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+durationofsong <- 
+  
 
 #### Save data ####
 write_csv(cleaned_data, "outputs/data/analysis_data.csv")
